9b572a5a2d4e17a728423a8738643c4ad3b1e92b
Bruno Mera <thenunix@gmail.com>
2023-03-23 12:22:51 -0300

    Initial Commit

---
+++ main.lua
@@ -0,0 +1,55 @@
+local grid = {}
+camera = {x = 0,y = 0}
+
+function camera:Move(dt)
+    if love.keyboard.isDown("w") then
+        self.y = self.y - 140*dt
+    end
+
+    if love.keyboard.isDown("s") then
+        self.y = self.y + 140*dt
+    end
+
+    if love.keyboard.isDown("a") then
+        self.x = self.x - 140*dt
+    end
+
+    if love.keyboard.isDown("d") then
+        self.x = self.x + 140*dt
+    end
+end
+
+local function generateNoiseGrid()
+	-- Fill each tile in our grid with noise.
+	local baseX = 10000 * love.math.random()
+	local baseY = 10000 * love.math.random()
+	for y = 1, 160 do
+		grid[y] = {}
+		for x = 1, 160 do
+			grid[y][x] = love.math.noise(baseX+.1*x, baseY+.2*y)
+		end
+	end
+end
+
+function love.load()
+	generateNoiseGrid()
+end
+function love.keypressed(key)
+	if key == "space" then
+        generateNoiseGrid()
+    end
+end
+
+function love.update(dt)
+    camera:Move(dt)
+end
+
+function love.draw()
+	local tileSize = 8
+	for y = 1, #grid do
+		for x = 1, #grid[y] do
+			love.graphics.setColor(1, 1, 1, grid[y][x])
+			love.graphics.rectangle("fill", x*tileSize-camera.x, y*tileSize-camera.y, tileSize, tileSize)
+		end
+	end
+end
